26695b757ddbd63b48e320a529ab874d8194dafc
FLYFAN <oncwnuHv6d0vSjvoRm6Eps864IiY@git.weixin.qq.com>
2019-12-03 21:04:51 +0800

    Update

--- simple_operate.py
+++ simple_operate.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-import time as timer
+import time
 from operation.login import AccoutLoginer as Loginer
 import re
 
@@ -8,19 +8,21 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(1)
 driver.maximize_window()
 
-info = ['天津大学', '3018233042', '3018233042']
+account = '3018233042'
+
 # login
-loginer = Loginer(driver, info)
+loginer = Loginer(driver=driver, info=['天津大学', account, account])
 loginer.process()
 
-timer.sleep(1)
+time.sleep(1)
 # click to enter the target course
 driver.find_element_by_xpath(
     '/html/body/div[1]/div/div[1]/div[3]/div/div').click()
 
 for i in range(75):
-    # force to sleep for a healthy life
-    timer.sleep(1)
+    # force to sleep for a healthy livelihood
+    time.sleep(1)
+
     # get elements after rerender
     try:
         li = driver.find_element_by_xpath("//li[@class='listview-item']")
@@ -28,15 +30,22 @@ for i in range(75):
         print(e)
         # if not able to find a active li then break the loop
         break
-    # click to enter lesson of the <li> item
+
+    # click to enter lesson in the <li> item
     driver.execute_script('arguments[0].click();', li)
-    timer.sleep(1)
+    time.sleep(1)
+
     # get url args
-    url_args = re.findall(r'\?(.+)$', driver.current_url)
+    url_args = re.findall(r'\?(.+)$', driver.current_url)[0]
     # post finish msg to backend
     js = "window.fetch('https://weiban.mycourse.cn/pharos/usercourse/finish.do?{}')".format(
-        url_args[0])
+        url_args)
+    # post with js:fetch()
     driver.execute_script(js)
-    timer.sleep(1)
+    print('post ~args:{}'.format(url_args))
+    time.sleep(1)
+
     # navigate back
     driver.back()
+
+print('progress end')
